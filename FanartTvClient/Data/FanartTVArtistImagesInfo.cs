a1f90ddd8845b303f36f9148638c5269b8fd50ba
pc-david\david.desmaisons <david.desmaisons@way2.com.br>
2016-06-14 13:21:31 -0300

    Adding artist image info

---
+++ FanartTvClient/Data/FanartTVArtistImagesInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace FanartTvClient.Data 
+{
+    public class FanartTVArtistImagesInfo : FanartTVImagesInfo
+    {
+        public FanartTVImageInfo[] artistbackground { get; set; }
+        public FanartTVImageInfo[] artistthumb { get; set; }
+        public FanartTVImageInfo[] musiclogo { get; set; }
+        public FanartTVImageInfo[] hdmusiclogo { get; set; }
+        public FanartTVImageInfo[] musicbanner { get; set; }
+    }
+}
